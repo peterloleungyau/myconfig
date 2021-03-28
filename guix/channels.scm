da24343d2464d48878c48ccb955a8f30c582ff0f
Peter Lo <peterloleungyau@gmail.com>
2021-03-29 00:07:23 +0800

    Updated packages, and split into two profiles.
    One for default, one for data science stuffs.

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,8 +1,18 @@
 (list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (commit
+          "51dc6fb07ea1984f2ce55a44b0ce998200fb0e5c")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
         (name 'guix)
         (url "https://git.sjtu.edu.cn/sjtug/guix.git")
         (commit
-          "2283baae907f4f38a8299d47ba4c0b2b49222883")
+          "0efd68681dcec50d445a4fd080c315b999164828")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
